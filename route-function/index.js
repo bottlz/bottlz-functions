0810fb37d4358bc1e7630368127537f306d35c04
Meghana Srivastava <mms453@cornell.edu>
2022-11-13 15:38:30 -0500

    Modifying route function to expect coordinates in order [lon, lat]

--- route-function/index.js
+++ route-function/index.js
@@ -1,13 +1,33 @@
 require("dotenv").config();
 const maps = require("azure-maps-rest");
 
-async function getRoute(startPoint, endPoint, routeURL) {
-    //Start and end point input to the routeURL
-    //calculateRouteDirections expects coordinates in the order [longitude, latitude]
-    var coordinates = [[startPoint.coordinates[1], startPoint.coordinates[0]], [endPoint.coordinates[1], endPoint.coordinates[0]]];
+/**
+ * 
+ * Given a route, extracts the MultiLineString that represents the actual
+ * route itself. 
+ * @param {FeatureCollection} route 
+ * @returns Value stored at route.features[0].geometry.coordinates[0]
+ */
+function extractRoute(route) {
+    return route.features[0].geometry.coordinates[0];
+}
 
+/**
+ * 
+ * @param {GeoJSON.geometry} startPoint  
+ * @param {GeoJSON.geometry} endPoint 
+ * @param {RouteURL} routeURL 
+ * @returns Array of coordinates representing the route between startPoint 
+ * and endPoint
+ */
+async function getRoute(startPoint, endPoint, routeURL) {
+    //calculateRouteDirections expects coordinates in the order [longitude, latitude]
+    var coordinates = [startPoint.coordinates, endPoint.coordinates];
+
+    //TODO: add better error handling when route can't be generated
     return await routeURL.calculateRouteDirections(maps.Aborter.none, coordinates).then((directions) => {
-        return directions.geojson.getFeatures();
+        var route = directions.geojson.getFeatures();
+        return extractRoute(route);
     });
 }
 
@@ -24,7 +44,7 @@ module.exports = async function (context, req) {
     if (!bottleId || !origin || !endpoint || !created || !routes) {
         context.res = {
             status: 400,
-            body: "Missing required inputs to route function."
+            body: 'Missing required inputs to route function.'
         };
         return;
     }
@@ -38,12 +58,13 @@ module.exports = async function (context, req) {
         // TODO: pick actual endpoints
         var endpt1 = {
             "type": "Point",
-            "coordinates": [42.445119924722725, -76.49976358014023]
+            "coordinates": [-76.49976358014023, 42.445119924722725,]
         };
         var route1 = await getRoute(origin, endpt1, routeURL);
+        console.log(route1);
         newEndpt = {
             "type": "Point",
-            "coordinates": [42.44188967828053, -76.4873610457749]
+            "coordinates": [-76.4873610457749, 42.44188967828053]
         };
         var route2 = await getRoute(endpt1, newEndpt, routeURL);
         routes.push(route1, route2);
@@ -51,13 +72,12 @@ module.exports = async function (context, req) {
         // Only generate one new route
         newEndpt = {
             "type": "Point",
-            "coordinates": [42.445119924722725, -76.49976358014023]
+            "coordinates": [-76.49976358014023, 42.445119924722725]
         };
         var route1 = await getRoute(endpoint, newEndpt, routeURL);
         routes.push(route1);
     }
 
-    //TODO: only add part of routes that we actually need to database
     context.bindings.outputDocument = JSON.stringify({
         id: bottleId,
         created: created,
