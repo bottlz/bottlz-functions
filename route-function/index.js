d2dee4fd7714c1988e18e81672943c37d340080f
Benjamin Shen <bfs45@cornell.edu>
2022-11-25 21:47:29 -0500

    Update routes response to return bottle data, set up format and lint (#6)
    * return bottle data as response body, format
    
    * configure prettier
    
    * configure eslint

--- route-function/index.js
+++ route-function/index.js
@@ -1,124 +1,142 @@
 require("dotenv").config();
 const maps = require("azure-maps-rest");
 
-
 /**
- * 
+ *
  * Given a route represented as a GeoJSON FeatureCollection, extracts the array
- * of coordinates storing the route itself. 
- * @param {FeatureCollection} route 
+ * of coordinates storing the route itself.
+ * @param {FeatureCollection} route
  * @returns Value stored at route.features[0].geometry.coordinates[0]
  */
 function extractRoute(route) {
-    return route.features[0].geometry.coordinates[0];
+  return route.features[0].geometry.coordinates[0];
 }
 
 /**
- * Given a route represented as a GeoJSON FeatureCollection, extracts its 
+ * Given a route represented as a GeoJSON FeatureCollection, extracts its
  * length in meters.
- * @param {FeatureCollection} route 
- * @returns 
+ * @param {FeatureCollection} route
+ * @returns
  */
 function extractRouteLength(route) {
-    return route.features[0].properties.summary.lengthInMeters;
+  return route.features[0].properties.summary.lengthInMeters;
 }
 
 /**
  * Generates a route between startPoint and endPoint.
- * @param {GeoJSON.geometry} startPoint  
- * @param {GeoJSON.geometry} endPoint 
- * @param {RouteURL} routeURL 
- * @returns Array of coordinates representing the route between startPoint 
+ * @param {GeoJSON.geometry} startPoint
+ * @param {GeoJSON.geometry} endPoint
+ * @param {RouteURL} routeURL
+ * @returns Array of coordinates representing the route between startPoint
  * and endPoint
  */
 async function getRoute(startPoint, endPoint, routeURL) {
-    //calculateRouteDirections expects coordinates in the order [longitude, latitude]
-    var coordinates = [startPoint.coordinates, endPoint.coordinates];
+  //calculateRouteDirections expects coordinates in the order [longitude, latitude]
+  const coordinates = [startPoint.coordinates, endPoint.coordinates];
 
-    //TODO: add better error handling when route can't be generated
-    return await routeURL.calculateRouteDirections(maps.Aborter.timeout(10000), coordinates).then((directions) => {
-        var route = directions.geojson.getFeatures();
-        return { "distance": extractRouteLength(route), "route": extractRoute(route) };
+  //TODO: add better error handling when route can't be generated
+  return await routeURL
+    .calculateRouteDirections(maps.Aborter.timeout(10000), coordinates)
+    .then((directions) => {
+      const route = directions.geojson.getFeatures();
+      return {
+        distance: extractRouteLength(route),
+        route: extractRoute(route),
+      };
     });
 }
 
 /**
- * 
+ *
  * @returns Random number between -0.01 and 0.01
  */
 function offset() {
-    var offset = Math.random();
-    offset *= Math.round(Math.random()) ? 1 : -1;
-    offset /= 100;
-    return offset;
+  let offset = Math.random();
+  offset *= Math.round(Math.random()) ? 1 : -1;
+  offset /= 100;
+  return offset;
 }
 
 /**
  * Returns a random point within a 500 mile radius of the given origin.
  * @param {Coordinate array} origin
- * @param {SearchURL} searchURL 
+ * @param {SearchURL} searchURL
  */
 async function getRandomEndpoint(origin, searchURL) {
-    var lon = origin.coordinates[0] + offset();
-    var lat = origin.coordinates[1] + offset();
-    var radius = 500; // radius in meters to search in
+  const lon = origin.coordinates[0] + offset();
+  const lat = origin.coordinates[1] + offset();
+  const radius = 500; // radius in meters to search in
 
-    return await searchURL.searchNearby(maps.Aborter.timeout(10000), [lon, lat], {
-        limit: 1,
-        radius: radius
-    }).then((results) => {
-        var data = results.geojson.getFeatures();
-        return data.features[0].geometry;
+  return await searchURL
+    .searchNearby(maps.Aborter.timeout(10000), [lon, lat], {
+      limit: 1,
+      radius: radius,
+    })
+    .then((results) => {
+      const data = results.geojson.getFeatures();
+      return data.features[0].geometry;
     });
 }
-
 
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+  context.log("JavaScript HTTP trigger function processed a request.");
 
-    const bottleId = req.body && req.body.id;
-    const created = req.body && req.body.created;
-    const origin = req.body && req.body.origin;
-    const endpoint = req.body && req.body.endpoint;
-    var routes = req.body && req.body.routes;
+  const bottleId = req.body && req.body.id;
+  const created = req.body && req.body.created;
+  const origin = req.body && req.body.origin;
+  const endpoint = req.body && req.body.endpoint;
+  const routes = req.body && req.body.routes;
 
-    if (!bottleId || !origin || !endpoint || !created || !routes) {
-        context.res = {
-            status: 400,
-            body: 'Missing required inputs to route function.'
-        };
-        return;
-    }
+  if (!bottleId || !origin || !endpoint || !created || !routes) {
+    context.res = {
+      status: 400,
+      body: {
+        error: "Missing required inputs to route function.",
+      },
+    };
+    return;
+  }
 
-    var pipeline = maps.MapsURL.newPipeline(new maps.SubscriptionKeyCredential(process.env.MAPS_SUB_KEY));
-    var routeURL = new maps.RouteURL(pipeline);
-    var searchURL = new maps.SearchURL(pipeline);
+  try {
+    const pipeline = maps.MapsURL.newPipeline(
+      new maps.SubscriptionKeyCredential(process.env.MAPS_SUB_KEY)
+    );
+    const routeURL = new maps.RouteURL(pipeline);
+    const searchURL = new maps.SearchURL(pipeline);
 
-    var newEndpt;
+    const newEndpoint = await getRandomEndpoint(origin, searchURL);
     if (routes.length == 0) {
-        // No routes have been generated yet, generate two starting at origin
-        var endpt1 = await getRandomEndpoint(origin, searchURL);
-        var route1 = await getRoute(origin, endpt1, routeURL);
-        newEndpt = await getRandomEndpoint(origin, searchURL);
-        var route2 = await getRoute(endpt1, newEndpt, routeURL);
-        routes.push(route1, route2);
+      // No routes have been generated yet, generate two starting at origin
+      const firstEndpoint = await getRandomEndpoint(origin, searchURL);
+      const route1 = await getRoute(origin, firstEndpoint, routeURL);
+      const route2 = await getRoute(firstEndpoint, newEndpoint, routeURL);
+      routes.push(route1, route2);
     } else {
-        // Only generate one new route
-        newEndpt = await getRandomEndpoint(origin, searchURL);
-        var route1 = await getRoute(endpoint, newEndpt, routeURL);
-        routes.push(route1);
+      // Only generate one new route
+      const route = await getRoute(endpoint, newEndpoint, routeURL);
+      routes.push(route);
     }
 
-    context.bindings.outputDocument = JSON.stringify({
-        id: bottleId,
-        created: created,
-        origin: origin,
-        endpoint: newEndpt,
-        routes: routes
-    });
+    const bottleData = {
+      id: bottleId,
+      created,
+      origin,
+      endpoint: newEndpoint,
+      routes,
+    };
+
+    context.bindings.outputDocument = JSON.stringify(bottleData);
 
     context.res = {
-        status: 200, /* Defaults to 200 */
-        body: 'Successfully generated route for bottle ' + bottleId + '.'
+      status: 200,
+      body: bottleData,
     };
-}
+  } catch (err) {
+    context.res = {
+      status: 500,
+      body: {
+        error: err.message,
+      },
+    };
+  }
+};
